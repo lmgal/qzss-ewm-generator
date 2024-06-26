19d37159001c0bff91ae91fc84be46645ca5af0b
Louie Gallos <lmgallos@up.edu.ph>
2024-06-26 16:49:10 +0800

    Initial commit. Setup with Vite, React, Tailwind, TS, and Leaflet

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { MapContainer, TileLayer } from 'react-leaflet'
+import { useState } from 'react'
+import 'leaflet/dist/leaflet.css'
+
+
+function App() {
+
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default App
