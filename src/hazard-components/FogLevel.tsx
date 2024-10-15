d37f4332411c8e4ecea51e59b13a26a7996abf18
lmgal <lmgallos@up.edu.ph>
2024-10-15 19:41:51 +0800

    Fixed fog levels having incorrect array values

--- src/hazard-components/FogLevel.tsx
+++ src/hazard-components/FogLevel.tsx
@@ -13,6 +13,11 @@ const fogLevels = [
   'Level 7/7. Major accident.',
 ]
 const fogLevelDescriptions = [
+   'On land, object appear hazy or blurry. Road and rail traffic is unhindered. On sea, horizon cannot be seen. Lights and landmarks can be seen at working distances.',
+  'On land, railroad traffic takes additional caution. On sea, Lights on passing vessel are generally not distinct at distances under 1 mile. Fog signal are sounded.',
+  'On land, rail and road traffic is obstructed. On sea, Lights on passing vessels are generally not distinct at distances under 1 mile. Fog signals are sounded. On river, navigation is unhindered but extre caution is required.',
+  'On land, rail and road traffic impeded. On sea, lights on ships and other vessels cannot be seen at distances of 4 miles or less. On river, navigation is suspended',
+  'On land, all traffic is impeded and totally disorganized. On sea, lights on ships and other vessels cannot be seen at distances of 4 miles or less. On river, navigation is suspended.',
 ]
 
 
